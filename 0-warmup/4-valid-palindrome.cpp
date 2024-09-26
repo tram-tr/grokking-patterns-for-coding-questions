cba4a135b38b9526b6223cb82091861b43a91c64
Trâm T. <97485876+tram-tr@users.noreply.github.com>
2024-09-26 11:16:53 -0400

    4-valid-palindrome.cpp

---
+++ 0-warmup/4-valid-palindrome.cpp
@@ -0,0 +1,21 @@
+// 092624
+
+#include <iostream>
+
+class Solution {
+public:
+  bool isPalindrome(std::string s) {
+    // TODO: Write your code here
+    int start = 0;
+    for (int end = s.length()-1; end >= 0; end--) {
+      if (start < end && isalnum(s[end])) {
+        while (start < end && !isalnum(s[start]))
+          start++;
+        if (tolower(s[start]) != tolower(s[end]))
+          return false;
+        start++;
+      }
+    }
+    return true;
+  }
+};
